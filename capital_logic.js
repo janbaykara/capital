56cc788b84a5d3671142da2111c67d3b7c78be25
Jan Baykara <jan@baykara.co.uk>
2017-03-29 01:12:19 +0100

    Added births, deaths, age

--- capital_logic.js
+++
@@ -1,4 +0,0 @@
-// In the beginning...
-for(var i = 1, x = 5; i <= x; i++) {
-	Society.population.push(new Worker());
-}
--- capital_models.js
+++ capital_models.js
@@ -3,9 +3,17 @@ var Society = new Vue({
 	data: {
 		population: [],
 		commodities: 0,
-		day: 0
+		day: 0,
+		adultAge: 16,
+		menopause: 55,
+		lifeExpectancy: 75,
+		chanceOfConception: 10,
+		chanceOfDeath: 0.1
 	},
 	computed: {
+		averageAge: function() {
+			return _.meanBy(this.population, 'age');
+		},
 		LabourPowerTotal: function() {
 			return _.sumBy(this.population, 'LabourPowerIndividual');
 		},
@@ -20,7 +28,7 @@ var Society = new Vue({
 		// And everyday after...
 		setInterval(function() {
 			Society.newDay();
-		}, 500)
+		}, 200);
 	},
 	methods: {
 		newDay: function() {
@@ -29,16 +37,28 @@ var Society = new Vue({
 			_.forEach(Society.population, function(person) {
 				person.live();
 			});
-		}
+		},
+		// catastrophe: function() {
+		// 	if(_.random(0,100) < 0.0001) {
+		// 		_.(this.population).each(function(pop) {
+		// 			if(_.random(0,100) < _.random(0,100)) {
+		// 				pop.die();
+		// 			}
+		// 		})
+		// 	}
+		// }
 	}
 });
 
-var Worker = Vue.extend({
+var Human = Vue.extend({
 	data: function() {
 		return {
+			age: 0,
+			generation: 0,
+			Noffspring: 0,
 			firstname: _.sample(firstnames),
 			lastname: _.sample(lastnames),
-			LabourPowerIndividual: _.random(1,1.5) // fraction of commodity (1)
+			LabourPowerIndividual: Society.LabourPowerSocAvgUnit * _.random(0.75,1.25) || 1 // fraction of commodity (1)
 		};
 	},
 	computed: {
@@ -49,36 +69,98 @@ var Worker = Vue.extend({
 	},
 	methods: {
 		produce: function() {
-			Society.commodities += this.LabourPowerIndividual * this.hoursWorked;
-
-			// Incentive to beat the competition to market
-			// # Needs rationalising
-			// # Needs relativising - variable incentive to improve dependent on how much weaker
-			// # Needs investivising - variable ability (£ accrued) to improve dependent on how much weaker
+			// # Incentive to beat the competition to market
+			// # Separate into produce + sell
+			if(this.age >= Society.adultAge) {
+				Society.commodities += this.LabourPowerIndividual * this.hoursWorked;
+			}
+		},
+		consume: function () {
+			// # Needs payment/exchange of social wealth
+			// # Babies should eat less
+			// # Poor (i.e. underproducers) should eat less
+			Society.commodities -= 1;
+		},
+		improve: function() {
+			// # Underproducers should have more incentive to improve
+			// # Overproducers should be able to improve easier (£ investment)
+			// # Link to age (older, less likely)
 			if(this.LabourPowerIndividual < Society.LabourPowerSocAvgUnit) {
-				prodIncChance = 10
+				prodIncChance = 5
 			} else {
-				prodIncChance = 3
+				prodIncChance = 1
 			}
 
-			if(_.random(0,100) < 1) {
-				prodInc = 1
+			if(this.age >= Society.adultAge && _.random(0,100) < 0.05) { // Eureka!
+				prodInc = 0.1
 				console.log(this.name+": \"Eureka!\"")
 			} else {
-				prodInc = 0.1
+				prodInc = 0.01
 			}
 
 			// Random chance of productivity increase
 			if(_.random(0,100) < prodIncChance) this.LabourPowerIndividual += prodInc;
 		},
-		consume: function () {
-			// # Needs payment/exchange of social wealth
-			Society.commodities -= 1;
-		},
 		// # Reproduce, based on commodities available, to a limit
+		reproduce: function() {
+			if(
+				(Society.commodities/Society.population.length) >= Society.adultAge
+				&& Society.population.length >= 2
+				&& this.age >= (Society.adultAge * _.random(0.8,1.2))
+				&& this.age < (Society.menopause * _.random(0.8,1.2))
+			) {
+				if(_.random(0,100) < Society.chanceOfConception) {
+					var x = new Human();
+					x.generation = this.generation + 1;
+					this.Noffspring++;
+					if(x.generation > 3 && _.random(0,100) < 50) {
+						x.lastname = this.lastname;
+					}
+					console.log(this.name+" gave had child no."+this.Noffspring+": "+x.firstname+"! :)")
+					Society.population.push(x);
+				}
+			}
+		},
+		die: function() {
+			var index = Society.population.indexOf(this);
+			if(index != -1) Society.population.splice(index, 1);
+		},
 		live: function() {
-			this.produce();
-			this.consume();
+			// Die chance
+			if(_.random(0,100) < Society.chanceOfDeath) { // Natural causes
+				console.log(this.name+" died unexpectedly! :o")
+				this.die();
+			} else // Starvation
+			if(Society.commodities <= 0) {
+				if(_.random(0,100) < 10) {
+					console.log(this.name+" died of starvation :(")
+					this.die();
+				}
+			} else // Old age
+			if(this.age > Society.lifeExpectancy) {
+				if(_.random(0,100) < 10) {
+					console.log(this.name+" died naturally at the old age of "+this.age)
+					this.die();
+				}
+			} else {
+				this.age++;
+				this.consume();
+				this.produce();
+				this.improve();
+
+				// Reproduce chance
+				if( (Society.commodities/Society.population.length) >= Society.adultAge ) {
+					this.reproduce();
+				}
+			}
 		}
 	}
 });
+
+// In the beginning...
+for(var i = 1, x = 10; i <= x; i++) {
+	var newHuman = new Human();
+	newHuman.age = _.random(Society.adultAge, 50);
+	Society.population.push(newHuman);
+}
+Society.commodities = Society.averageAge * Society.adultAge;
